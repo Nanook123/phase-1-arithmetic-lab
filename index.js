48152c3fd865232516e84d1e015fb5f4b9d4c871
Joshua Allford <joshallfordnanook@gmail.com>
2021-09-02 13:37:11 +0100

    commit tree ed7c04b3679966f36510c87bc0507fe17d548089

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+console.log(multiply);
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+console.log(random);
+
+let num3 = 8;
+let num4 = 2;
+
+let mod = num3 / num4;
+
+console.log(mod)
+
+const max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20)
+
+console.log(max)
